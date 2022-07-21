5af0432b2ee9d89727ee1cf0d20a8a4ff80f6bf2
Longhl <850420399@qq.com>
2022-07-21 09:49:11 +0800

    first commit

---
+++ src/test/java/com/lhl/JuniorJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
